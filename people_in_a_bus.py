600adc6020a6f5123f650b38f62be0db5887760d
maskasumina <mariiasumina5@gmail.com>
2023-05-16 19:46:54 +0200

    Added new programs

---
+++ people_in_a_bus.py
@@ -0,0 +1,11 @@
+ans = 0
+def number(bus_stops):
+    ans = bus_stops[i[0]]
+    for i in range(len(bus_stops)):
+        if 0 == i.int:
+            ans = ans - bus_stops[i[1]].int
+        elif 0 != i.int:
+            ans = ans - bus_stops[i[1]].int
+    return ans
+
+print(number())
