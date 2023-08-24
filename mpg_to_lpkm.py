e497bd7265fad3af6b06c3b40ef5d030f6ef83fe
maskasumina <mariiasumina5@gmail.com>
2023-08-24 15:38:04 +0200

    Added new programs

---
+++ mpg_to_lpkm.py
@@ -0,0 +1,3 @@
+def converter(mpg):
+    ans = (mpg / 4.54609188) * 1.609344
+    return round(ans,2)
---
+++ pipe_fix.py
@@ -0,0 +1,5 @@
+def pipe_fix(nums):
+    output = []
+    for i in range(nums[0], nums[-1]+1):
+        output.append(i)
+    return output
